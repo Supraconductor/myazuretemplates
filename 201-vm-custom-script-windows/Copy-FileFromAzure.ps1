77ff87e38626aa72d030110a95a639ec43dd70df
Supraconductor <jeanfrdore@live.ca>
2016-11-17 02:07:49 -0500

    storage name

--- 201-vm-custom-script-windows/Copy-FileFromAzure.ps1
+++ 201-vm-custom-script-windows/Copy-FileFromAzure.ps1
@@ -1,9 +1,10 @@
 param (
-    [string]$pass
+    [string]$shareName,
+    [string]$storageAccountName    
 )
-$securepassword = ConvertTo-SecureString -string $pass -AsPlainText -Force
+<# $securepassword = ConvertTo-SecureString -string $pass -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PsCredential('blobanalyticsgtd',$securepassword)
-<# New-PSDrive -Name "S" -Scope Global -PSProvider FileSystem -Root "\\blobanalyticsgtd.file.core.windows.net\dsvmshare" -Persist -Credential $cred
+New-PSDrive -Name "S" -Scope Global -PSProvider FileSystem -Root "\\blobanalyticsgtd.file.core.windows.net\dsvmshare" -Persist -Credential $cred
 cmdkey /add:blobanalyticsgtd.file.core.windows.net /user:blobanalyticsgtd /pass:$pass #>
 New-Item 'C:\Users\default\Desktop\mapdrive.cmd' -type file -force 
-Set-Content 'C:\Users\default\Desktop\mapdrive.cmd' "net use s: \\blobanalyticsgtd.file.core.windows.net\dsvmshare /u:blobanalyticsgtd $pass"
+Set-Content 'C:\Users\default\Desktop\mapdrive.cmd' "net use s: \\$storageAccountName.file.core.windows.net\dsvmshare /u:$shareName /persistent:yes"
