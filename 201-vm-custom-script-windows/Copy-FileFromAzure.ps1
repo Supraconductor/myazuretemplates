af335b0941e93b3e04a9b41d491e61d3d0f7b7bd
Supraconductor <jeanfrdore@live.ca>
2016-11-16 22:49:57 -0500

    bad car

--- 201-vm-custom-script-windows/Copy-FileFromAzure.ps1
+++ 201-vm-custom-script-windows/Copy-FileFromAzure.ps1
@@ -1,7 +1,7 @@
 <# Custom Script for Windows to install a file from Azure Storage using the staging folder created by the deployment script #>
 param (
-    [string]$pass
+    [System.Security.SecureString]$pass
 )
 
 $cred = New-Object System.Management.Automation.PsCredential('blobanalyticsgtd',$pass)
-New-PSDrive –Name "S" –PSProvider FileSystem –Root "\\blobanalyticsgtd.file.core.windows.net\dsvmshare" –Persist 
+New-PSDrive -Name "S" -PSProvider FileSystem -Root "\\blobanalyticsgtd.file.core.windows.net\dsvmshare" -Persist -Credential $cred
