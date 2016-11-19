a12ddeca218db709c4329efc0318d096317571f3
Supraconductor <jeanfrdore@live.ca>
2016-11-19 11:22:52 -0500

    runbooks

--- dsvm/deploy.ps1
+++ dsvm/deploy.ps1
@@ -1,85 +1,70 @@
 <#
- .SYNOPSIS
-    Deploys a template to Azure
+    .DESCRIPTION
+        An example runbook which gets all the ARM resources using the Run As Account (Service Principal)
 
- .DESCRIPTION
-    Deploys an Azure Resource Manager template
-
- .PARAMETER subscriptionId
-    The subscription id where the template will be deployed.
-
- .PARAMETER resourceGroupName
-    The resource group where the template will be deployed. Can be the name of an existing or a new resource group.
-
- .PARAMETER resourceGroupLocation
-    Optional, a resource group location. If specified, will try to create a new resource group in this location. If not specified, assumes resource group is existing.
-
- .PARAMETER deploymentName
-    The deployment name.
-
- .PARAMETER templateFilePath
-    Optional, path to the template file. Defaults to template.json.
-
- .PARAMETER parametersFilePath
-    Optional, path to the parameters file. Defaults to parameters.json. If file is not found, will prompt for parameter values based on template.
+    .NOTES
+        AUTHOR: Azure Automation Team
+        LASTEDIT: Mar 14, 2016
 #>
-
 param(
- [Parameter(Mandatory=$True)]
- [string]
- $subscriptionId,
-
  [Parameter(Mandatory=$True)]
  [string]
  $resourceGroupName,
 
  [string]
- $resourceGroupLocation,
+ $resourceGroupLocation = "eastus2",
+
+ [string]
+ $CommonResourceGroupName = "sndbx-comm-rg",
+
+ [Parameter(Mandatory=$True)]
+ [PSCredential]
+ $adminCredential,
 
  [Parameter(Mandatory=$True)]
  [string]
- $deploymentName,
+ $sandboxName,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $groupName,
 
  [string]
- $templateFilePath = "template.json",
+ $subnetName = "sndbx",
 
  [string]
- $parametersFilePath = "parameters.json"
+ $virtualNetworkName = "sndbx-comm-vnet",
+ 
+ [string]
+ $virtualMachineSize = "Standard_DS3_v2"
+
 )
 
-<#
-.SYNOPSIS
-    Registers RPs
-#>
-Function RegisterRP {
-    Param(
-        [string]$ResourceProviderNamespace
-    )
+#$templateFilePath = "https://raw.githubusercontent.com/Supraconductor/myazuretemplates/master/dsvm/template.json"
+#blob avec politique acces container
+$templateFilePath = "https://sndbxcomtempl.blob.core.windows.net/templates/template.json"
 
-    Write-Host "Registering resource provider '$ResourceProviderNamespace'";
-    Register-AzureRmResourceProvider -ProviderNamespace $ResourceProviderNamespace;
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
 }
-
-#******************************************************************************
-# Script body
-# Execution begins here
-#******************************************************************************
-$ErrorActionPreference = "Stop"
-
-# sign in
-Write-Host "Logging in...";
-Login-AzureRmAccount;
-
-# select subscription
-Write-Host "Selecting subscription '$subscriptionId'";
-Select-AzureRmSubscription -SubscriptionID $subscriptionId;
-
-# Register RPs
-$resourceProviders = @("microsoft.compute","microsoft.storage","microsoft.network");
-if($resourceProviders.length) {
-    Write-Host "Registering resource providers"
-    foreach($resourceProvider in $resourceProviders) {
-        RegisterRP($resourceProvider);
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
     }
 }
 
@@ -92,16 +77,21 @@ if(!$resourceGroup)
         $resourceGroupLocation = Read-Host "resourceGroupLocation";
     }
     Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
-    New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Tag @{Name="groupe";Value=$groupName}
 }
 else{
     Write-Host "Using existing resource group '$resourceGroupName'";
 }
 
-# Start the deployment
-Write-Host "Starting deployment...";
-if(Test-Path $parametersFilePath) {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
-} else {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath;
-}
+$params = @{adminPassword=$adminCredential.GetNetworkCredential().Password;GroupeDeTravail=$groupName;SandboxName=$sandboxName;adminUsername=$adminCredential.UserName;CommonResourceGroupName=$CommonResourceGroupName;subnetName=$subnetName;virtualNetworkName=$virtualNetworkName;virtualMachineSize=$virtualMachineSize}
+
+$results = New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterObject $params;
+$results;
+
+# create a context for account and key
+$ctx = New-AzureStorageContext $results.Outputs.sharedStorageAccountName.Value $results.Outputs.key.Value
+$s = New-AzureStorageShare partage -Context $ctx
+
+
+
+
