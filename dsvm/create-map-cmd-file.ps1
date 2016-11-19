b07d06e37681ab9d1790c4057d5aa3964009e76c
Supraconductor <jeanfrdore@live.ca>
2016-11-18 23:19:44 -0500

    cmdkey

--- dsvm/create-map-cmd-file.ps1
+++ dsvm/create-map-cmd-file.ps1
@@ -7,4 +7,6 @@ $cred = New-Object System.Management.Automation.PsCredential('blobanalyticsgtd',
 New-PSDrive -Name "S" -Scope Global -PSProvider FileSystem -Root "\\blobanalyticsgtd.file.core.windows.net\dsvmshare" -Persist -Credential $cred
 cmdkey /add:blobanalyticsgtd.file.core.windows.net /user:blobanalyticsgtd /pass:$pass #>
 New-Item 'C:\Users\default\Desktop\mapdrive.cmd' -type file -force 
-Set-Content 'C:\Users\default\Desktop\mapdrive.cmd' "net use s: \\$storageAccountName.file.core.windows.net\dsvmshare /u:$shareName /persistent:yes"
+Set-Content 'C:\Users\default\Desktop\mapdrive.cmd' "SET /P key=Please enter storage account key: "
+Add-Content 'C:\Users\default\Desktop\mapdrive.cmd' "cmdkey /add:$storageAccountName.file.core.windows.net /user:$shareName /pass:%key%"
+Add-Content 'C:\Users\default\Desktop\mapdrive.cmd' "net use s: \\$storageAccountName.file.core.windows.net\dsvmshare /u:$shareName /persistent:yes"
