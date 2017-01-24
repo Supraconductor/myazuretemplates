5337144df35a5df7ab6ac3c5b04710ec46c9db52
Supraconductor <jeanfrdore@live.ca>
2017-01-23 21:33:27 -0500

    disk creation

--- dsvm-v2/StoragePool.ps1
+++ dsvm-v2/StoragePool.ps1
@@ -6,7 +6,7 @@ Configuration StoragePool
   {
 	Script ConfigureStoragePool { 
 		SetScript = { 
-			$disks = Get-PhysicalDisk
+			$disks = (Get-PhysicalDisk -FriendlyName PhysicalDisk2)
 			New-StoragePool -FriendlyName "DataPool" -StorageSubsystemFriendlyName "Storage Spaces*" -PhysicalDisks $disks | New-VirtualDisk -FriendlyName "DataDisk" -UseMaximumSize -ResiliencySettingName "Simple" -ProvisioningType Fixed | Initialize-Disk -Confirm:$False -PassThru | New-Partition -DriveLetter P -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "DATA" -Confirm:$false			
 		} 
 
